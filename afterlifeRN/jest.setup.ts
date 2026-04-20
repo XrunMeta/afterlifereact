7c80bd7f0216d75d8c35d315a5af7e5dbed2c21d
xrun <xrun@xrun.run>
2026-04-20 18:06:36 +0900

    feat(rn): rewrite Step1 as cloneType selection (2x2)

--- afterlifeRN/jest.setup.ts
+++ afterlifeRN/jest.setup.ts
@@ -4,6 +4,26 @@ jest.mock('react-native/Libraries/Alert/Alert', () => ({
   alert: jest.fn(),
 }));
 
+jest.mock('@expo/vector-icons', () => {
+  const { View } = require('react-native');
+  return {
+    Feather: View,
+    AntDesign: View,
+    MaterialIcons: View,
+    Ionicons: View,
+  };
+});
+
+jest.mock('react-native-safe-area-context', () => {
+  const { View } = require('react-native');
+  return {
+    SafeAreaProvider: ({ children }: any) => children,
+    SafeAreaView: View,
+    useSafeAreaInsets: () => ({ top: 0, right: 0, bottom: 0, left: 0 }),
+    useSafeAreaFrame: () => ({ x: 0, y: 0, width: 390, height: 844 }),
+  };
+});
+
 jest.mock('expo-image-picker', () => ({
   launchImageLibraryAsync: jest.fn(),
 }));
