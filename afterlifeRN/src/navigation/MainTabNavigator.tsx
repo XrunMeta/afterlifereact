733ada7d7b149479b73144ae69e8b542a433ac9c
Doongiho <happyluck0426@gmail.com>
2026-08-06 16:17:44 +0900

    fix(app): 선물함을 별도 화면 X → 크레딧 충전 화면(Purchase) 하단에 통합
    사용자 자료 재검토 결과 별도 '선물 지갑' 화면이 아니라
    '크레딧 충전·구독' 화면의 하단에 꽃/보약 [교환] 목록이 들어가는 구조.
    
    변경:
    - PurchaseScreen: 받은 선물 섹션 추가 (whitelist oth-test@example.invalid), 카드 리스트 + [교환] 버튼
    - GiftInventoryScreen.tsx 삭제
    - MyScreen 의 '선물 지갑' 메뉴 항목 삭제
    - MainTabNavigator / navigation types 에서 GiftInventory 등록 제거
    
    API 클라이언트(giftInventory.ts) 는 유지. Swap 로직은 PurchaseScreen 내부에 이식.

--- afterlifeRN/src/navigation/MainTabNavigator.tsx
+++ afterlifeRN/src/navigation/MainTabNavigator.tsx
@@ -40,7 +40,6 @@ import InviteStatusScreen from "../screens/my/InviteStatusScreen";
 import BlockedListScreen from "../screens/my/BlockedListScreen";
 import ReportsScreen from "../screens/my/ReportsScreen";
 import TransactionsScreen from "../screens/my/TransactionsScreen";
-import GiftInventoryScreen from "../screens/my/GiftInventoryScreen";
 
 import PurchaseScreen from "../screens/my/PurchaseScreen";
 
@@ -102,8 +101,6 @@ function MyStackNavigator() {
       <MyStack.Screen name="Transactions" component={TransactionsScreen} />
       <MyStack.Screen name="Purchase" component={PurchaseScreen} />
       <MyStack.Screen name="Reports" component={ReportsScreen} />
-      {}
-      <MyStack.Screen name="GiftInventory" component={GiftInventoryScreen} />
     </MyStack.Navigator>
   );
 }
