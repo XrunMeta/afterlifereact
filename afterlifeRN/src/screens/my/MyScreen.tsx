77cc988724fa9cc8cf0a8f5155c0c5c6add13bfc
Doongiho <114303137+Doongiho@users.noreply.github.com>
2026-08-10 18:14:37 +0900

    fix(rn): T-467 실험 파이프라인 flag oth-user@example.invalid (oth-test@example.invalid → 교체) (#875)

--- afterlifeRN/src/screens/my/MyScreen.tsx
+++ afterlifeRN/src/screens/my/MyScreen.tsx
@@ -261,7 +261,7 @@ export default function MyScreen() {
       },
     },
 
-    ...(["oth-staff@example.invalid", "oth-test@example.invalid"].includes(apiUser?.email ?? "")
+    ...(["oth-staff@example.invalid", "oth-user@example.invalid"].includes(apiUser?.email ?? "")
       ? [
           {
             icon: "cpu" as const,
